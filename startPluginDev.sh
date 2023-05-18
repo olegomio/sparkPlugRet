40f6c043f1a29b32a9c4248fa72f885a43190f52
oleg.omio <geiger.ral@gmail.com>
2023-05-18 21:32:42 +0200

    add plugin name func, mod quit command, add dir name output

--- startPluginDev.sh
+++ startPluginDev.sh
@@ -3,25 +3,41 @@
 
 # functions
 
+set_plugin_name() {
+
+    echo "Enter name of plugin:"
+
+    while [[ -z $pluginname || $pluginname == ' ' ]]; do
+
+        echo $invalid_empty
+        read pluginname
+
+    done
+
+    if [[ $pluginname == '-q' ]]; then
+        exit 1
+    fi
+
+}
+
 set_author_name() {
     local q1validation=false
     local q2validation=false
-    local invalid="Invalid input. Please enter 'y' or 'n'."
 
-    while [ $q1validation == false ]; do
+    while [[ $q1validation == false && (-z $oauthor || $oauthor == ' ') ]]; do
 
         echo "Are you the author?(y/n)"
         read oauthor
 
-        if [ "$oauthor" == "y" ]; then 
+        if [[ "$oauthor" == "y" ]]; then 
             q1validation=true
 
-            while [ $q2validation == false ]; do
+            while [[ $q2validation == false ]]; do
 
                 echo "do you want to use your git user.name?"
                 read usegit
 
-                if [ "$usegit" == "y" ]; then 
+                if [[ "$usegit" == "y" ]]; then 
 
                     local gituser=$(git config user.name)
 
@@ -32,46 +48,80 @@ set_author_name() {
                     echo "The git user name is: $gituser, setting author name to $username"
                     q2validation=true
 
-                elif [ "$usegit" == "n" ];  then
+                elif [[ "$usegit" == "n" ]]; then
 
                     echo "please type in your name:"
                     read username
 
                     echo "setting author name to $username"
                     q2validation=true
+                
+                elif [[ "$usegit"  == '-q' ]]; then
+                    exit 1
                 else
 
-                    echo $invalid
+                    echo $invalid_yes_no
                     continue
                 
                 fi
             done
 
-        elif [ "$oauthor" == "n" ]; then
+        elif [[ "$oauthor" == "n" ]]; then
 
             echo "Enter author name:"
             read authorname
+
+            echo "setting author name to $authorname"
             q1validation=true
 
+        elif [[ "$oauthor" == '-q' ]]; then
+            exit 1
         else
 
-            echo $invalid
+            echo $invalid_yes_no
             continue
 
         fi
     done
 }
 
+validate_inputs() {
+
+    #echo "Plugin Name: $pluginname // Author Name: $authorname"
+
+    # concatenate plugin name for dir name
+    dirname=$(echo "retarus_$pluginname" | sed -e 's/ /_/g' -e 's/\(.\)/\L\1/g')
+
+    echo "Setting directory name to $dirname"
+
+}
+
+#create_dirs_and_files() {
+
+    
+
+   # echo $dirname 
+
+#}
+
+
 #create_file_structure( $pluginname, $username ) {
 
 #}
 
 
-# prompt user for filename
-echo "Enter name of plugin:"
-read pluginname
+# console outputs
 
+invalid_yes_no="Invalid input. Please enter 'y', 'n' or '-q' to quit."
+invalid_empty="Invalid input. Your prompt is empty. Please enter something or '-q' to quit."
+
+# script sequence
+
+set_plugin_name
 set_author_name
+validate_inputs
+create_dirs_and_files
+
 
 # create new base file
 
